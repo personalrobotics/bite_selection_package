50555f52c3ae8b8d7f8e6d06cb25e2355df3805c
Youngsun Kim <yskim041@gmail.com>
2018-06-06 14:11:55 -0700

    update setup script

--- setup_labelImg.sh
+++ setup_labelImg.sh
@@ -2,6 +2,7 @@
 
 if [ "$1" == "python3" ]; then
   echo "installing labelImg with Python3 + Qt5"
+  git submodule update --init --recursive
   cd ./external_apps/labelImg
   sudo apt install pyqt5-dev-tools && \
       sudo pip3 install lxml && \
@@ -11,6 +12,7 @@ if [ "$1" == "python3" ]; then
 
 elif [ "$1" == "python2" ]; then
   echo "installing labelImg with Python2 + Qt4"
+  git submodule update --init --recursive
   cd ./external_apps/labelImg
   sudo apt install pyqt4-dev-tools && \
     sudo pip2 install lxml && \
