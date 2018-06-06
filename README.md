8c78914e4204af2a12525414401a44caad4e6a69
Youngsun Kim <yskim041@gmail.com>
2018-06-06 14:03:31 -0700

    add helper scripts and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # bite_selection_package
+
+## Making 2D bounding boxes by using labelImg
+To build labelImg, run `setup_labelImg.sh` with a python version (python2 or python3) you want to use. For example,
+```
+./setup_labelImg.sh python3
+```
+It will install pyqt-dev-tools and python-lxml, and build labelImg.
+
+If you successfully built labelImg, you can start it by typing:
+```
+./run_labelImg.sh
+```
+
+All the images are in `data/bounding_boxes/images`, and annotations will be saved in `data/bounding_boxes/annotations/xmls`.
+
+## Recording skewering positions
+WIP
+
---
+++ setup_labelImg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$1" == "python3" ]; then
+  echo "installing labelImg with Python3 + Qt5"
+  cd ./external_apps/labelImg
+  sudo apt install pyqt5-dev-tools && \
+      sudo pip3 install lxml && \
+      make qt5py3
+  cd ../../
+  mkdir config && cd config && echo "python3" > python_version && cd ../
+
+elif [ "$1" == "python2" ]; then
+  echo "installing labelImg with Python2 + Qt4"
+  cd ./external_apps/labelImg
+  sudo apt install pyqt4-dev-tools && \
+    sudo pip2 install lxml && \
+    make qt4py2
+  cd ../../
+  mkdir config && cd config && echo "python2" > python_version && cd ../
+
+else
+  echo "please specify python version you want to use"
+  echo "usage:"
+  echo "    ./setup_labelImg.sh python3 or (python2)"
+fi
+
