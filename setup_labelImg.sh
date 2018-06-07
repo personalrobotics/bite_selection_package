450c7191a1ada08ebde7240409843e5b88e39348
Youngsun Kim <yskim041@gmail.com>
2018-06-07 16:12:16 -0700

    update os checking in the setup script

--- setup_labelImg.sh
+++ setup_labelImg.sh
@@ -12,55 +12,60 @@ fi
 
 git submodule update --init --recursive
 
-if [ "$OSTYPE" == "linux-gnu" ]; then
-  if [ "$1" == "python3" ]; then
-    echo "installing labelImg with Python3 + Qt5"
-    cd ./external_apps/labelImg
-    sudo apt install python3-dev python3-pip pyqt5-dev-tools && \
-        sudo pip3 install lxml && \
-        make qt5py3
-    cd ../../
-    mkdir config && cd config && echo "python3" > python_version && cd ../
+uname_out="$(uname -s)"
+case "${uname_out}" in
+    Linux*)
+      if [ "$1" == "python3" ]; then
+        echo "installing labelImg with Python3 + Qt5"
+        cd ./external_apps/labelImg
+        sudo apt install python3-dev python3-pip pyqt5-dev-tools && \
+            sudo pip3 install lxml && \
+            make qt5py3
+        cd ../../
+        mkdir config && cd config && echo "python3" > python_version && cd ../
 
-  elif [ "$1" == "python2" ]; then
-    echo "installing labelImg with Python2 + Qt4"
-    cd ./external_apps/labelImg
-    sudo apt install python-dev python-pip pyqt4-dev-tools && \
-      sudo pip2 install lxml && \
-      make qt4py2
-    cd ../../
-    mkdir config && cd config && echo "python2" > python_version && cd ../
+      elif [ "$1" == "python2" ]; then
+        echo "installing labelImg with Python2 + Qt4"
+        cd ./external_apps/labelImg
+        sudo apt install python-dev python-pip pyqt4-dev-tools && \
+          sudo pip2 install lxml && \
+          make qt4py2
+        cd ../../
+        mkdir config && cd config && echo "python2" > python_version && cd ../
 
-  else
-    print_usage
-  fi
+      else
+        print_usage
+      fi
+      ;;
 
-elif [ "$OSTYPE" == "darwin"* ]; then
-  if [ "$1" == "python3" ]; then
-    echo "installing labelImg with Python3 + Qt5"
-    cd ./external_apps/labelImg
-    brew install qt && \
-        brew install libxml2 && \
-        pip3 install PyQt5 lxml && \
-        make qt5py3
-    cd ../../
-    mkdir config && cd config && echo "python3" > python_version && cd ../
+    Darwin*)
+      if [ "$1" == "python3" ]; then
+        echo "installing labelImg with Python3 + Qt5"
+        cd ./external_apps/labelImg
+        brew install qt && \
+            brew install libxml2 && \
+            python3 -m pip install PyQt5 lxml && \
+            make qt5py3
+        cd ../../
+        mkdir config && cd config && echo "python3" > python_version && cd ../
 
-  elif [ "$1" == "python2" ]; then
-    echo "installing labelImg with Python2 + Qt4"
-    cd ./external_apps/labelImg
-    brew install qt4 && \
-        brew install libxml2 && \
-        pip2 install PyQt4 lxml && \
-        make qt4py2
-    cd ../../
-    mkdir config && cd config && echo "python2" > python_version && cd ../
+      elif [ "$1" == "python2" ]; then
+        echo "installing labelImg with Python2 + Qt4"
+        cd ./external_apps/labelImg
+        brew install qt4 && \
+            brew install libxml2 && \
+            python2 -m pip install PyQt4 lxml && \
+            make qt4py2
+        cd ../../
+        mkdir config && cd config && echo "python2" > python_version && cd ../
 
-  else
-    print_usage
-  fi
+      else
+        print_usage
+      fi
+      ;;
 
-else
-  echo "This script does not support \"${OSTYPE}\"."
-fi
+    *)
+      echo "This script does not support \"${uname_out}\"."
+      ;;
+esac
 
