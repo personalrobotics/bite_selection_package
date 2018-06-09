9192e2b42a6163fdb0acd7036d31c6bbe60f2610
Youngsun Kim <yskim041@gmail.com>
2018-06-08 18:01:57 -0700

    replace space to _

---
+++ data/bounding_boxes/annotations/update_path.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import xml.etree.ElementTree as ET
+import os
+
+
+image_base_dir = '/mnt/hard_data/Data/foods/data/images'
+
+
+def update_xml(filepath, filename, is_edit=False):
+    print('>>> {}'.format(filename))
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+
+    for node in root:
+        if node.tag == 'object':
+            name_node = node.find('name')
+            # name_node.text = name_node.text.replace(' ', '_')
+            print('{} -> {}'.format(
+                name_node.text,
+                name_node.text.replace(' ', '_')))
+
+    if is_edit:
+        tree.write(filepath)
+
+
+def update_all_xmls(base_dir, is_edit=False):
+    filenames = os.listdir(base_dir)
+
+    trainval_list = list()
+
+    for filename in filenames:
+        filepath = os.path.join(base_dir, filename)
+        update_xml(filepath, filename, is_edit)
+        trainval_list.append(filename[:-4])
+
+    f_tv = open('trainval.txt.new', 'w')
+    for trainval in trainval_list:
+        f_tv.write('%s\n' % trainval)
+    f_tv.close()
+
+
+def script_main():
+    print('update_path')
+
+    import sys
+    is_edit = False
+    if (len(sys.argv) == 2) and (sys.argv[1] == 'true'):
+        is_edit = True
+    update_all_xmls('xmls', is_edit=is_edit)
+
+    print('edit mode: ', is_edit)
+
+
+if __name__ == '__main__':
+    script_main()
+
+
+# End of script
+
