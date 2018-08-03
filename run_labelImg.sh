f143a95c511f16ed6458032ef048988f06ede7df
Youngsun Kim <yskim041@gmail.com>
2018-08-03 16:40:38 -0700

    update script run_labelImg.sh

--- run_labelImg.sh
+++ run_labelImg.sh
@@ -4,6 +4,6 @@ python_version=`cat ./config/python_version`
 
 $python_version ./external_apps/labelImg/labelImg.py \
     ./data/bounding_boxes/images \
-    ./external_apps/labelImg/data/predefined_classes.txt \
+    ./config/predefined_classes_foods.txt \
     ./data/bounding_boxes/annotations/xmls
 
