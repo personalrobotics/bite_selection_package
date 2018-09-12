d8685028fd785b28eacd27ab5b79739da6d193f8
Youngsun Kim <yskim041@gmail.com>
2018-09-11 20:39:38 -0700

    add config for different angle_reses

--- config/config.py
+++ config/config.py
@@ -7,45 +7,54 @@ gpu_id = '0'
 
 use_identity = False
 use_rotation = True
-use_rot_alt = True
+use_rot_alt = False
 use_densenet = True
 
-valid_denseblock_sizes = [[3, 6], [6, 12]]
-denseblock_sizes = valid_denseblock_sizes[1]
+valid_denseblock_sizes = [[3, 6], [6, 12], [12, 12]]
+denseblock_sizes = valid_denseblock_sizes[0]
 
 project_dir = os.path.split(os.getcwd())[0]
 
-project_prefix = 'food_spnet_c8'
+num_classes = 8
+mask_size = 17  # grid_shape: (17, 17)
+angle_res = 18
+
+project_prefix = 'food_spnet_c{}'.format(num_classes)
 if use_identity:
     project_prefix += '_identity'
 if use_densenet:
     project_prefix += '_dense_{}_{}'.format(*denseblock_sizes)
 if not use_rotation:
     project_prefix += '_loc_only'
-if use_rotation and use_rot_alt:
-    project_prefix += '_rot_alt'
+else:
+    if use_rot_alt:
+        project_prefix += '_rot_alt'
+    else:
+        project_prefix += '_a_{}'.format(angle_res)
 
-num_classes = 8
-
-mask_size = 17  # grid_shape: (17, 17)
-angle_res = 18
 
 # project_prefix += 'a_{}'.format(angle_res)
 
 cropped_img_res = mask_size * 8  # 136
 
-train_batch_size = 16
+train_batch_size = 32
 test_batch_size = 4
 
 dataset_dir = os.path.join(project_dir, 'data')
 
-label_map_filename = os.path.join(dataset_dir, 'food_c8_label_map.pbtxt')
-img_dir = os.path.join(dataset_dir, 'bounding_boxes_c8/images')
-cropped_img_dir = os.path.join(dataset_dir, 'skewering_positions_c8/cropped_images')
-mask_dir = os.path.join(dataset_dir, 'skewering_positions_c8/masks')
+label_map_filename = os.path.join(
+    dataset_dir, 'food_c{}_label_map.pbtxt'.format(num_classes))
+img_dir = os.path.join(
+    dataset_dir, 'bounding_boxes_c{}/images'.format(num_classes))
+cropped_img_dir = os.path.join(
+    dataset_dir, 'skewering_positions_c{}/cropped_images'.format(num_classes))
+mask_dir = os.path.join(
+    dataset_dir, 'skewering_positions_c{}/masks'.format(num_classes))
 
-train_list_filename = os.path.join(dataset_dir, 'food_c8_ann_train.txt')
-test_list_filename = os.path.join(dataset_dir, 'food_c8_ann_test.txt')
+train_list_filename = os.path.join(
+    dataset_dir, 'food_c{}_ann_train.txt'.format(num_classes))
+test_list_filename = os.path.join(
+    dataset_dir, 'food_c{}_ann_test.txt'.format(num_classes))
 
 pretrained_dir = os.path.join(project_dir, 'pretrained')
 pretrained_filename = os.path.join(
