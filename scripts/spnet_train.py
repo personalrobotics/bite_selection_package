ee551ef3e49c9be8e27f93fec4fdb6e9eb2ad00a
Youngsun Kim <yskim041@gmail.com>
2018-09-13 11:07:25 -0700

    add balanced loss

--- scripts/spnet_train.py
+++ scripts/spnet_train.py
@@ -90,7 +90,7 @@ def train_spnet(use_cuda=True):
         spnet = DenseSPNet()
     else:
         spnet = SPNet()
-    print(spnet)
+    # print(spnet)
 
     best_loss = float('inf')
     start_epoch = 0
@@ -120,9 +120,12 @@ def train_spnet(use_cuda=True):
         spnet.train()
         # spnet.module.freeze_bn()
         train_loss = 0
-        this_tbp = 0
-        this_tbr = 0
-        this_trd = 0
+
+        this_bacc = 0
+        this_bpre = 0
+        this_brec = 0
+        this_bf1 = 0
+        this_rdist = 0
 
         total_batches = int(math.ceil(
             trainloader.dataset.num_samples / trainloader.batch_size))
@@ -147,21 +150,26 @@ def train_spnet(use_cuda=True):
 
             train_loss += loss.data
 
-            bmask_precision, bmask_recall, rmask_dist = get_accuracy(
+            bacc, bpre, brec, bf1, rdist = get_accuracy(
                 pred_bmasks, gt_bmasks, pred_rmasks, gt_rmasks)
-            this_tbp += bmask_precision
-            this_tbr += bmask_recall
-            this_trd += rmask_dist
+            this_bacc += bacc
+            this_bpre += bpre
+            this_brec += brec
+            this_bf1 += bf1
+            this_rdist += rdist
 
             if batch_idx % 20 == 0:
-                print('[{0}| {1:3d}/{2:3d}] test_loss: {3:6.3f} '
+                print('[{0}| {1:3d}/{2:3d}] train_loss: {3:6.3f} '
                       '(b={4:.3f}, r={5:.3f}) | avg_loss: {6:6.3f} | '
-                      'bp: {7:.4f}, br: {8:.4f}, rd: {9:.4f}'.format(
+                      'bacc: {7:.3f}, bpre: {8:.3f}, brec: {9:.3f}, '
+                      'bf1: {10:.3f}, rdist: {11:.3f}'.format(
                     epoch, batch_idx, total_batches, loss.data,
                     bmloss, rmloss, train_loss / (batch_idx + 1),
-                    this_tbp / (batch_idx + 1),
-                    this_tbr / (batch_idx + 1),
-                    this_trd / (batch_idx + 1)))
+                    this_bacc / (batch_idx + 1),
+                    this_bpre / (batch_idx + 1),
+                    this_brec / (batch_idx + 1),
+                    this_bf1 / (batch_idx + 1),
+                    this_rdist / (batch_idx + 1)))
 
                 # # save a sample ground truth data
                 # ann_path_base = os.path.join(
@@ -187,9 +195,12 @@ def train_spnet(use_cuda=True):
         print('\nTest')
         spnet.eval()
         test_loss = 0
-        this_tbp = 0
-        this_tbr = 0
-        this_trd = 0
+
+        this_bacc = 0
+        this_bpre = 0
+        this_brec = 0
+        this_bf1 = 0
+        this_rdist = 0
 
         total_batches = int(math.ceil(
             testloader.dataset.num_samples / testloader.batch_size))
@@ -210,21 +221,26 @@ def train_spnet(use_cuda=True):
 
             test_loss += loss.data
 
-            bmask_precision, bmask_recall, rmask_dist = get_accuracy(
+            bacc, bpre, brec, bf1, rdist = get_accuracy(
                 pred_bmasks, gt_bmasks, pred_rmasks, gt_rmasks)
-            this_tbp += bmask_precision
-            this_tbr += bmask_recall
-            this_trd += rmask_dist
+            this_bacc += bacc
+            this_bpre += bpre
+            this_brec += brec
+            this_bf1 += bf1
+            this_rdist += rdist
 
             if batch_idx % 20 == 0:
-                print('[{0}| {1:3d}/{2:3d}] test_loss: {3:6.3f} '
+                print('[{0}| {1:3d}/{2:3d}]  test_loss: {3:6.3f} '
                       '(b={4:.3f}, r={5:.3f}) | avg_loss: {6:6.3f} | '
-                      'bp: {7:.4f}, br: {8:.4f}, rd: {9:.4f}'.format(
+                      'bacc: {7:.3f}, bpre: {8:.3f}, brec: {9:.3f}, '
+                      'bf1: {10:.3f}, rdist: {11:.3f}'.format(
                     epoch, batch_idx, total_batches, loss.data,
                     bmloss, rmloss, test_loss / (batch_idx + 1),
-                    this_tbp / (batch_idx + 1),
-                    this_tbr / (batch_idx + 1),
-                    this_trd / (batch_idx + 1)))
+                    this_bacc / (batch_idx + 1),
+                    this_bpre / (batch_idx + 1),
+                    this_brec / (batch_idx + 1),
+                    this_bf1 / (batch_idx + 1),
+                    this_rdist / (batch_idx + 1)))
 
                 # # save a sample prediction
                 # img_path_base = os.path.join(
