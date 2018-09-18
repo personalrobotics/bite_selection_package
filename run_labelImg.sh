3cfd70967763f427b967381782cf02b184404128
Youngsun Kim <yskim041@gmail.com>
2018-09-17 18:19:31 -0700

    add c7_dataset

--- run_labelImg.sh
+++ run_labelImg.sh
@@ -3,7 +3,7 @@
 python_version=`cat ./config/python_version`
 
 python3 ./external_apps/labelImg/labelImg.py \
-    ./data/bounding_boxes_c8/images \
-    ./config/predefined_classes_foods_c8.txt \
-    ./data/bounding_boxes_c8/annotations/xmls
+    ./data/bounding_boxes_c7/images \
+    ./config/predefined_classes_foods_c7.txt \
+    ./data/bounding_boxes_c7/annotations/xmls
 
