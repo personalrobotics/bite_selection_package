f28b45e3a4fdcaf80fdf6eec1b5b251990daa2a5
Youngsun Kim <yskim041@gmail.com>
2019-02-23 20:12:29 -0800

    add load_checkpoint script for test

--- data/load_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-scp prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/Data/foods/bite_selection_package/data/food_label_map.pbtxt $DIR
-
---
+++ load_checkpoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "load spnet checkpoints from mthrbrn"
+if [ ! -d "./checkpoint" ]; then
+  mkdir ./checkpoint
+fi
+scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/Checkpoints/bite_selection_package/checkpoint/spnet_ckpt.pth ./checkpoint/
+
+echo "load label_map definition from mthrbrn"
+if [ ! -d "./data" ]; then
+  mkdir ./data
+fi
+scp prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/Data/foods/bite_selection_package/food_detector/food_label_map.pbtxt ./data/
+
