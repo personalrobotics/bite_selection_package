89cfdd662d03809d53ed2fe2cc7413d0b3339da6
Gilwoo Lee <gilwoo301@gmail.com>
2019-05-02 15:02:16 -0700

    Add script for downloading spanet_wall_ckpt

--- load_checkpoint.sh
+++ load_checkpoint.sh
@@ -6,6 +6,9 @@ if [ ! -d "./checkpoint" ]; then
 fi
 scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/Checkpoints/bite_selection_package/checkpoint/spnet_ckpt.pth ./checkpoint/
 
+echo "load spanet checkpoints from mthrbrn"
+scp prl@mthrbrn.personalrobotics.cs.washington.edu:/home/ekgordon/Workspace/isrr_ws/src/bite_selection_package/checkpoint/food_spanet_all_rgb_wall_ckpt_best.pth ./checkpoint/spanet_with_wall_ckpt.pth
+
 echo "load label_map definition from mthrbrn"
 if [ ! -d "./data" ]; then
   mkdir ./data
