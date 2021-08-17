f44f1f30738e50722fa851177258d859d5638baa
Ethan K. Gordon <ekgordon@cs.washington.edu>
2021-08-17 13:31:08 -0700

    New ckpt location

--- load_checkpoint.sh
+++ load_checkpoint.sh
@@ -4,10 +4,11 @@ echo "load spnet checkpoints from mthrbrn"
 if [ ! -d "./checkpoint" ]; then
   mkdir ./checkpoint
 fi
-scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/Checkpoints/bite_selection_package/checkpoint/spnet_ckpt.pth ./checkpoint/
+scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/Checkpoints/public/spnet_ckpt.pth ./checkpoint/
 
 echo "load spanet checkpoints from mthrbrn"
-scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/Checkpoints/bite_selection_package/checkpoint/spanet_ckpt.pth ./checkpoint/
+scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/Checkpoints/public/spanet_ckpt.pth ./checkpoint/
+scp -r prl@mthrbrn.personalrobotics.cs.washington.edu:/mnt/hard_data/Checkpoints/public/spanet_wall.pth ./checkpoint/
 
 echo "load label_map definition from mthrbrn"
 if [ ! -d "./data" ]; then
